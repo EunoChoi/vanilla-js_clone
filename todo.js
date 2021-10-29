c0b58fc86f05fb83f48e2d68aa5a772a47c634c8
EunoChoi <64246481+EunoChoi@users.noreply.github.com>
2021-10-30 03:37:09 +0900

    add popup window
    add popup window
    - show list text

--- todo.js
+++ todo.js
@@ -1,8 +1,21 @@
 const todoForm = document.querySelector("#todo_form");
-
+const list = document.querySelector("#todolist li");
+const popup = document.querySelector("#popup");
+const popupClose = document.querySelector("#popup_close");
 
 function input(event){
     event.preventDefault();
     changBg();
 }
+
+function viewOpen(){
+    popup.style.display = "flex";
+}
+
+function viewClose(){
+    popup.style.display = "none";
+}
+
 todoForm.addEventListener("submit", input);
+list.addEventListener("click", viewOpen)
+popupClose.addEventListener("click", viewClose);
