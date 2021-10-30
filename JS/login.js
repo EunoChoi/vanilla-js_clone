e71da369199066bc73027aa95570c494e7119607
EunoChoi <64246481+EunoChoi@users.noreply.github.com>
2021-10-30 18:48:13 +0900

    add some animation
    add some animation

--- JS/login.js
+++ JS/login.js
@@ -2,6 +2,7 @@ const CLASS_HIDDEN = "hidden";
 const loginForm = document.querySelector("#login_form");
 const headerTitle = document.querySelector(".header h1");
 const inputName = document.querySelector("#input_name");
+const wellcomeArea = document.querySelector("#wellcome");
 
 loginForm.addEventListener("submit",submitLogin);
 
@@ -24,7 +25,13 @@ function loginByName(name){
 function submitLogin(event){
     event.preventDefault();
     loginByName(inputName.value);
-    
+
+    //after login changed header area
+    todoForm.classList.toggle(CLASS_HIDDEN);
+    wellcomeArea.classList.toggle(CLASS_HIDDEN);
+
+    headerArea.style.height = "300px";
+
     startInterval(timer,1000);
     //use local storage
     localStorage.setItem("user", inputName.value);
@@ -33,4 +40,8 @@ function submitLogin(event){
 if(localStorage.getItem("user")!=null){
     loginByName(localStorage.getItem("user"));
     startInterval(timer,1000);
+}
+else{
+    headerArea.style.height = "100vh";
+    todoForm.classList.toggle(CLASS_HIDDEN);
 }
