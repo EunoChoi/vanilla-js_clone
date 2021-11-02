973a926f52e6338e4749d63e4bea17ed25289c5c
EunoChoi <64246481+EunoChoi@users.noreply.github.com>
2021-11-02 16:59:44 +0900

    pwa done

--- JS/bgimg.js
+++ JS/bgimg.js
@@ -1,7 +1,6 @@
 const colors = ["mediumaquamarine", "lightblue","darkseagreen","lightpink","steelblue","slategrey"];
 const headerArea = document.querySelector(".header");
 const bottomArea= document.querySelector(".bottom");
-const popupTime = document.querySelector("#popup_time");
 
 const rand = Math.floor(Math.random() * colors.length);
 
@@ -10,7 +9,6 @@ function changBg(){
     if(headerArea.style.backgroundColor !== colors[rand]){
         headerArea.style.backgroundColor = colors[rand];
         bottomArea.style.backgroundColor = colors[rand];
-        popupTime.style.backgroundColor = colors[rand];
     }  
     else changBg();
 }
