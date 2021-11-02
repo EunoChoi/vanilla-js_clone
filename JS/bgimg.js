3a1c8feb1eeb9910a20185cd3e0991a7499bc6be
EunoChoi <64246481+EunoChoi@users.noreply.github.com>
2021-11-02 19:19:53 +0900

    popup css update

--- JS/bgimg.js
+++ JS/bgimg.js
@@ -1,17 +1,17 @@
-const colors = ["mediumaquamarine", "lightblue","darkseagreen","lightpink","steelblue","slategrey"];
+const colors = ["mediumaquamarine", "lightblue", "darkseagreen", "lightpink", "steelblue", "slategrey"];
 const headerArea = document.querySelector(".header");
-const bottomArea= document.querySelector(".bottom");
+const bottomArea = document.querySelector(".bottom");
 const popupTime = document.querySelector("#popup_time");
 
 const rand = Math.floor(Math.random() * colors.length);
 
-function changBg(){
+function changBg() {
     const rand = Math.floor(Math.random() * colors.length);
-    if(headerArea.style.backgroundColor !== colors[rand]){
+    if (headerArea.style.backgroundColor !== colors[rand]) {
         headerArea.style.backgroundColor = colors[rand];
         bottomArea.style.backgroundColor = colors[rand];
-        popupTime.style.backgroundColor = colors[rand];
-    }  
+        //popupTime.style.backgroundColor = colors[rand];
+    }
     else changBg();
 }
 changBg();
