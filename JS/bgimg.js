b77b8bd7ed62193788f05732f4a7ac77afe0db6a
EunoChoi <64246481+EunoChoi@users.noreply.github.com>
2021-11-03 04:37:19 +0900

    css fix, and bg color change method fix
    css fix, and bg color change method fix

--- JS/bgimg.js
+++ JS/bgimg.js
@@ -2,14 +2,20 @@ const colors = ["mediumaquamarine", "lightblue", "lightpink", "steelblue", "slat
 const headerArea = document.querySelector(".header");
 const bottomArea = document.querySelector(".bottom");
 const popupTime = document.querySelector("#popup_time");
+const deleteIcon = document.querySelector(".deleteicon");
 
 const rand = Math.floor(Math.random() * colors.length);
 
 function changBg() {
+    let currentColor;
     const rand = Math.floor(Math.random() * colors.length);
-    if (headerArea.style.backgroundColor !== colors[rand]) {
+
+    //to get different color everytime, using localstorage
+    if (localStorage.getItem("currentColor") !== colors[rand]) {
         headerArea.style.backgroundColor = colors[rand];
         bottomArea.style.backgroundColor = colors[rand];
+        deleteIcon.style.color = colors[rand];
+        localStorage.setItem("currentColor", colors[rand]);
         //popupTime.style.backgroundColor = colors[rand];
     }
     else changBg();
